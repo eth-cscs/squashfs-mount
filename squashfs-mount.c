294c18df94aa7ecbde0101ca55cb2124c93c0fa1
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-07-30 15:46:35 +0200

    call retrun_to_user_and_no_new_privs

--- squashfs-mount.c
+++ squashfs-mount.c
@@ -307,8 +307,7 @@ int main(int argc, char **argv) {
   fwd_argv = argv + (positional_args + 1);
   // if no mountpoints given, run command directly
   if (positional_args == 0) {
-    if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) != 0)
-      err(EXIT_FAILURE, "PR_SET_NO_NEW_PRIVS failed");
+    return_to_user_and_no_new_privs(uid);
     fprintf(stderr, "Warning no <image>:<mountpoint> argument was given.\n");
     char **new_env = fwd_env();
     if (new_env == NULL) {
